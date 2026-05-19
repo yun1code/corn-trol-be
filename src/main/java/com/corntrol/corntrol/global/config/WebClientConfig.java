f33937d800b9b8b68027152b04185582fe84ec6b
yun1code <ysy4797@gmail.com>
2026-05-19 12:01:56 +0900

    fix: WebClient 타임아웃 시간 3분으로 연장

--- src/main/java/com/corntrol/corntrol/global/config/WebClientConfig.java
+++ src/main/java/com/corntrol/corntrol/global/config/WebClientConfig.java
@@ -2,13 +2,22 @@ package com.corntrol.corntrol.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofMinutes(3));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
